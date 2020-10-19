659d07d8247cd3e2e109c91f5c2f80c46bddfa06
Julia Borovets <juliaborovets2001@gmail.com>
2020-10-20 01:17:32 +0300

    Implemented constructors and methods for ToDo class. #3

--- src/main/java/com/softserve/itacademy/model/ToDo.java
+++ src/main/java/com/softserve/itacademy/model/ToDo.java
@@ -1,9 +1,16 @@
 package com.softserve.itacademy.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class ToDo {
+
+    private static int counter = 0;
+
+    private Integer toDoId;
 
     private String title;
 
@@ -13,6 +20,75 @@ public class ToDo {
 
     private List<Task> tasks;
 
-    // Constructor(s), getters, setters, hashCode, equals, etc.
+    public ToDo(String title, LocalDateTime createdAt, User owner) {
+        this.toDoId = counter++;
+        this.title = title;
+        this.createdAt = createdAt;
+        this.owner = owner;
+        this.tasks = new LinkedList<>();
+    }
 
+    public Integer getToDoId() {
+        return toDoId;
+    }
+
+    public void setToDoId(Integer toDoId) {
+        this.toDoId = toDoId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ToDo)) return false;
+
+        ToDo toDo = (ToDo) o;
+
+        if (!Objects.equals(toDoId, toDo.toDoId)) return false;
+        if (!Objects.equals(title, toDo.title)) return false;
+        if (!Objects.equals(createdAt, toDo.createdAt)) return false;
+        if (!Objects.equals(owner, toDo.owner)) return false;
+        return Objects.equals(tasks, toDo.tasks);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = toDoId != null ? toDoId.hashCode() : 0;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
+        result = 31 * result + (owner != null ? owner.hashCode() : 0);
+        result = 31 * result + (tasks != null ? tasks.hashCode() : 0);
+        return result;
+    }
 }
